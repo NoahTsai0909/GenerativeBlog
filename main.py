b6477a40c67bb8461b7af19af73d555fe06895cd
Noah Tsai <mrtsai@ucdavis.edu>
2023-09-25 12:59:53 -0700

    Added input function

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
 print(tokenizer.eos_token_id)
 model = GPT2LMHeadModel.from_pretrained('gpt2-large', pad_token_id=tokenizer.eos_token_id)
 
-sentence = "Video games are addicting"
+sentence = input('Please enter the first sentence of the blog post as a prompt: \n')
+maxlen = input('Please enter the max word count for the blog post:\n')
 input_ids = tokenizer.encode(sentence, return_tensors="pt")
-print(input_ids)
 
-output = model.generate(input_ids, max_length = 100, num_beams=5, no_repeat_ngram_size=2, early_stopping=True)
+output = model.generate(input_ids, max_length = int(maxlen), num_beams=5, no_repeat_ngram_size=2, early_stopping=True)
 print(tokenizer.decode(output[0], skip_special_tokens=True))
