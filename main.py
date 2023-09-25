24599abbf1706dc32bc55dc7ecb1fee9fcb05610
Noah Tsai <mrtsai@ucdavis.edu>
2023-09-24 20:03:41 -0700

    implemented decoder for a paragraph

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
 print(tokenizer.eos_token_id)
 model = GPT2LMHeadModel.from_pretrained('gpt2-large', pad_token_id=tokenizer.eos_token_id)
 
-sentence = "I like ice cream"
+sentence = "Video games are addicting"
 input_ids = tokenizer.encode(sentence, return_tensors="pt")
 print(input_ids)
-print(tokenizer.decode(input_ids[0][2]))
+
+output = model.generate(input_ids, max_length = 100, num_beams=5, no_repeat_ngram_size=2, early_stopping=True)
+print(tokenizer.decode(output[0], skip_special_tokens=True))
